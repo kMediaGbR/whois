d6fd51ba9169ad10189002e2808038dea043a6e2
Sebastian Kalicki <sebastian@kalicki.email>
2020-08-10 00:43:43 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { WhoisService } from './service/whois.service';
+import { WhoisService } from './service/whois.service';
 
+export { WhoisService } from './service/whois.service';
+export const whois = (domain: string) => new WhoisService().request(domain);
